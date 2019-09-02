448ad1a9347334873070a0c88dba36005b26f6f7
Nhi Nguyen <nhn34@drexel.edu>
2019-09-02 00:30:07 -0400

    tried

--- js/scripts.js
+++ js/scripts.js
@@ -58,3 +58,50 @@ $navA.on( "click", burgerClick);
 /* ==============================
 Burger Animation End
 ============================== */
+
+$(document).ready(function() {
+
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        dots: true
+    });
+
+//     $('.carousel').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         mobileFirst: true,
+//         adaptiveHeight: true,
+//         responsive: [
+//             {
+//                 breakpoint: 1200,
+//                 settings: {
+//                     slidesToShow: 3,
+//                     slidesToScroll: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             },
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//             // You can unslick at a given breakpoint now by adding:
+//             // settings: "unslick"
+//             // instead of a settings object
+//         ]
+//     });
+ });
